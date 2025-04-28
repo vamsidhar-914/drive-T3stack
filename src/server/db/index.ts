cb3129c110d0f68d5c9c3892a0037751d8ef7da3
Kandula Vamshidhar Reddy <vamsidhar.kanudula@ibm.com>
2025-04-29 03:09:31 +0530

    database integrated with singlestore

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool, type Pool } from 'mysql2/promise'
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -9,11 +9,19 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  client: Pool | undefined;
 };
 
 export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
+  globalForDb.client ?? createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASSWORD,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle:0
+  });
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
