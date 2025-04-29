4d146679de731a20fef5b5baea76e713877e71ae
Kandula Vamshidhar Reddy <vamsidhar.kanudula@ibm.com>
2025-04-29 05:42:16 +0530

    db model working!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,26 @@
-import { singlestoreTable, int,text } from "drizzle-orm/singlestore-core";
+import { singlestoreTable, int,text, singlestoreTableCreator, bigint } from "drizzle-orm/singlestore-core";
+import { index } from "node_modules/drizzle-orm/singlestore-core";
+ 
+export const createTable = singlestoreTableCreator((name) => `drive-t3_${name}`)
 
-export const users = singlestoreTable("user_table", {
-  id: int("id").primaryKey().autoincrement(),
+export const files = createTable("files_table", {
+  id: bigint("id",{ mode: 'number',unsigned: true }).primaryKey().autoincrement(),
   name: text("name"),
-  age: int("age")
+  size: int("size"),
+  url: text("url"),
+  parent: bigint("parent",{ mode: 'number',unsigned: true }).notNull(),
+} , (t) => {
+  return [
+    index("parent_index").on(t.parent),
+  ]
 })
 
+export const folders = createTable("folders_table",{
+  id: bigint("id",{ mode: 'number', unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent",{ mode: 'number', unsigned: true })
+} , (t) => {
+  return [
+    index("parent_index").on(t.parent),
+  ]
+})
