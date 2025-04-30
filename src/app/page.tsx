d1619843b9fcd18948b7ba86e1ebc11e1d9042bd
Kandula Vamshidhar Reddy <vamsidhar.kanudula@ibm.com>
2025-04-30 06:49:08 +0530

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { db } from "~/server/db";
 import { files as filesSchema , folders as foldersSchema} from "~/server/db/schema";
 import DriveContents from "./drive-contents";
 
-export default async function GoogleDriveClone() {
-    const files = await db.select().from(filesSchema);
-    const folders = await db.select().from(foldersSchema);
-    return (
-       <DriveContents files={files} folders={folders} />
-    );
+export default function GoogleDriveClone() {
+    return(
+        <h3>this is just a home page</h3>
+    )
 }
